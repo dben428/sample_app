7757d265b8a5695e21a6ff04f717b07cbfe650d4
Dan Benedetti <danben428@aol.com>
2013-08-14 17:08:31 -0400

    Validate presence of user_id for micropost

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -10,5 +10,12 @@ describe Micropost do
   subject { @micropost }
   
   it { should respond_to(:content) }
-  it { should respond_to(:user_id) }  
+  it { should respond_to(:user_id) }
+
+  it { should be_valid }
+  
+  describe "when user id is not present" do
+    before { @micropost.user_id = nil }
+    it { should_not be_valid }
+  end
 end
